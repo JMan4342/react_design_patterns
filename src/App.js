dc4df7d4ae682f36b1ab3f195fddc3c367e16d8c
Joseph Manning <jgmanning3@yahoo.com>
2022-05-20 14:11:01 -0700

    Create DataSource component to allow access from different servers

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ import { RegularList } from "./RegularList";
 
 import { Modal } from "./Modal";
 
+import axios from "axios";
 import { CurrentUserLoader } from "./CurrentUserLoader";
 import { UserLoader } from "./UserLoader";
+import { DataSource } from "./DataSource";
 import { ResourceLoader } from "./ResourceLoader";
 import { UserInfo } from "./UserInfo";
 import { ProductInfo } from "./ProductInfo";
@@ -123,15 +125,26 @@ import { ProductInfo } from "./ProductInfo";
 // ***** CONTAINER COMPONENTS ******
 // ***** CONTAINER COMPONENTS ******
 
+const getServerData = (url) => async () => {
+  const response = await axios.get(url);
+  return response.data;
+};
+
+const getLocalStorageData = (key) => () => {
+  return localStorage.getItem(key);
+};
+
+const Text = ({ message }) => <h1>{message}</h1>;
+
 function App() {
   return (
     <>
-      <ResourceLoader resourceUrl="/users/123" resourceName="user">
+      <DataSource getDataFunc={getServerData("/users/123")} resourceName="user">
         <UserInfo />
-      </ResourceLoader>
-      <ResourceLoader resourceUrl="/products/1234" resourceName="product">
-        <ProductInfo />
-      </ResourceLoader>
+      </DataSource>
+      <DataSource getDataFunc={getLocalStorageData('message')} resourceName="message">
+        <Text />
+      </DataSource>
     </>
   );
 }
