16142ee62fd4e577573991f3583fbc3e3d7a32c7
Joseph Manning <jgmanning3@yahoo.com>
2022-05-25 16:18:46 -0700

    Create init printProps to use for higher-order component

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@ import { ControlledModal } from "./ControlledModal";
 import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow";
 import { ControlledOnboardingFlow } from "./ControlledOnboardingFlow";
 
+import { printProps } from "./printProps";
+
 // *** SPLITSCREEN DEMO ***
 // *** SPLITSCREEN DEMO ***
 
@@ -182,48 +184,61 @@ import { ControlledOnboardingFlow } from "./ControlledOnboardingFlow";
 // *****  CONTROLLED AND UNCONTROLLED ONBOARDING FLOW COMPONENTS  *****
 // *****  CONTROLLED AND UNCONTROLLED ONBOARDING FLOW COMPONENTS  *****
 
-const StepOne = ({ goToNext }) => (
-  <>
-    <h1>Step 1</h1>
-    <button onClick={() => goToNext({ name: "John Doe" })}>Next</button>
-  </>
-);
-const StepTwo = ({ goToNext }) => (
-  <>
-    <h1>Step 2</h1>
-    <button onClick={() => goToNext({ age: 100 })}>Next</button>
-  </>
-);
-const StepThree = ({ goToNext }) => (
-  <>
-    <h1>Step 3</h1>
-    <p>Congratulations! You qualify for our senior discount</p>
-    <button onClick={() => goToNext({})}>Next</button>
-  </>
-);
-const StepFour = ({ goToNext }) => (
-  <>
-    <h1>Step 4</h1>
-    <button onClick={() => goToNext({ hairColor: "brown" })}>Next</button>
-  </>
-);
+// const StepOne = ({ goToNext }) => (
+//   <>
+//     <h1>Step 1</h1>
+//     <button onClick={() => goToNext({ name: "John Doe" })}>Next</button>
+//   </>
+// );
+// const StepTwo = ({ goToNext }) => (
+//   <>
+//     <h1>Step 2</h1>
+//     <button onClick={() => goToNext({ age: 100 })}>Next</button>
+//   </>
+// );
+// const StepThree = ({ goToNext }) => (
+//   <>
+//     <h1>Step 3</h1>
+//     <p>Congratulations! You qualify for our senior discount</p>
+//     <button onClick={() => goToNext({})}>Next</button>
+//   </>
+// );
+// const StepFour = ({ goToNext }) => (
+//   <>
+//     <h1>Step 4</h1>
+//     <button onClick={() => goToNext({ hairColor: "brown" })}>Next</button>
+//   </>
+// );
+
+// function App() {
+//   const [onboardingData, setOnboardingData] = useState({});
+//   const [currentIndex, setCurrentIndex] = useState(0);
+
+//   const onNext = (stepData) => {
+//     setOnboardingData({ ...onboardingData, ...stepData });
+//     setCurrentIndex(currentIndex + 1);
+//   };
+
+//   return (
+//     <ControlledOnboardingFlow currentIndex={currentIndex} onNext={onNext}>
+//       <StepOne />
+//       <StepTwo />
+//       {onboardingData.age >= 62 && <StepThree />}
+//       <StepFour />
+//     </ControlledOnboardingFlow>
+//   );
+// }
+
+// *****  HIGHER-ORDER COMPONENTS *****
+// *****  HIGHER-ORDER COMPONENTS *****
+
+const UserInfoWrapped = printProps(UserInfo);
 
 function App() {
-  const [onboardingData, setOnboardingData] = useState({});
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const onNext = (stepData) => {
-    setOnboardingData({ ...onboardingData, ...stepData });
-    setCurrentIndex(currentIndex + 1);
-  };
-
   return (
-    <ControlledOnboardingFlow currentIndex={currentIndex} onNext={onNext}>
-      <StepOne />
-      <StepTwo />
-      {onboardingData.age >= 62 && <StepThree />}
-      <StepFour />
-    </ControlledOnboardingFlow>
+    <>
+      <UserInfoWrapped a={1} b="Hello" c={{ name: "Shaun" }} />
+    </>
   );
 }
 
